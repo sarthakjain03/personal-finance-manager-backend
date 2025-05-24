dd6dd5458adc771ecffd1907d581ff132355c1d3
Sarthak Jain <jainsarthak2003@gmail.com>
2025-05-24 22:41:36 +0530

    updated signup api and added login api with basic email validation

--- src/app.js
+++ src/app.js
@@ -1,20 +1,40 @@
 require("dotenv").config();
 const express = require("express");
+const validator = require("validator");
 const connectDB = require("./config/database");
-
 const User = require("./models/user");
 
 const app = express();
+app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-  const newUser = new User({
-    name: "Test User",
-    email: "test@test.com",
-    password: "test123",
-  });
-
   try {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      res.status(400).send({
+        success: false,
+        message: "Please provide all the required fields",
+      });
+    }
+
+    if (!validator.isEmail(email)) {
+      res.status(400).send({
+        success: false,
+        message: "Invalid email address",
+      });
+    }
+
+    const user = await User.findOne({ email });
+    if (user) {
+      res.status(400).send({
+        success: false,
+        message: "User already Registered",
+      });
+    }
+
+    const newUser = new User({ name, email, password });
     await newUser.save();
+
     res.send({
       success: true,
       message: "User created successfully!",
@@ -22,7 +42,51 @@ app.post("/signup", async (req, res) => {
   } catch (error) {
     res.status(400).send({
       success: false,
-      message: "Error creating user",
+      message: "Error creating user: " + error?.message,
+    });
+  }
+});
+
+app.get("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      res.status(400).send({
+        success: false,
+        message: "Please provide all the required fields",
+      });
+    }
+
+    if (!validator.isEmail(email)) {
+      res.status(400).send({
+        success: false,
+        message: "Invalid email address",
+      });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(404).send({
+        success: false,
+        message: "User is not Registered",
+      });
+    }
+    if (user?.password !== password) {
+      res.status(401).send({
+        success: false,
+        message: "Invalid Password",
+      });
+    }
+
+    res.send({
+      success: true,
+      message: "User Logged In Successfully",
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      message: "Error logging in: " + error?.message,
     });
   }
 });
--- src/models/user.js
+++ src/models/user.js
@@ -2,24 +2,33 @@ const mongoose = require("mongoose");
 
 // TODO: Add forgot password functionality
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profileImageUrl: {
+      type: String,
+      default:
+        "https://cdn.vectorstock.com/i/1000v/92/16/default-profile-picture-avatar-user-icon-vector-46389216.jpg",
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 
