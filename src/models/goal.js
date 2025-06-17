f7880492192dabd31b579e025d236fb5a775e47e
Sarthak Jain <jainsarthak2003@gmail.com>
2025-06-17 22:40:16 +0530

    added goal schema

--- src/models/goal.js
+++ src/models/goal.js
@@ -1,1 +1,46 @@
 const mongoose = require("mongoose");
+
+const goalSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 3,
+    maxLength: 50,
+  },
+  description: {
+    type: String,
+    trim: true,
+    maxLength: 250,
+  },
+  currentAmount: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  targetAmount: {
+    type: Number,
+    required: true,
+    min: 100,
+  },
+  category: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 2,
+    maxLength: 30,
+  },
+  deadline: {
+    type: Date,
+    required: true,
+  },
+});
+
+const Goal = mongoose.model("Goal", goalSchema);
+
+module.exports = Goal;
