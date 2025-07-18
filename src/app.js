3fe5a22899fd8afe4b942e94c2334c0aad1d5e73
Sarthak Jain <jainsarthak2003@gmail.com>
2025-07-18 21:27:33 +0530

    updated cors url

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://finsphere-frontend.vercel.app",
     credentials: true,
   })
 );
