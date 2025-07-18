8e7ca287a4399b82c170e110273ae8ff3c5610ed
Sarthak Jain <jainsarthak2003@gmail.com>
2025-07-18 23:26:22 +0530

    fix try

--- api/app.js
+++ api/app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const serverless = require("serverless-http");
+// const serverless = require("serverless-http");
 
 const connectDB = require("../config/database");
 const authRouter = require("../routes/auth");
@@ -46,13 +46,13 @@ app.use((err, req, res, next) => {
 connectDB()
   .then(() => {
     console.log("Connected to database successfully!");
-    // app.listen(7777, () => {
-    //   console.log("Server is running on port 7777...");
-    // });
+    app.listen(7777, () => {
+      console.log("Server is running on port 7777...");
+    });
   })
   .catch((error) => {
     console.error("Error connecting to database", error);
   });
 
-module.exports = app;
-module.exports.handler = serverless(app);
+// module.exports = app;
+// module.exports.handler = serverless(app);
